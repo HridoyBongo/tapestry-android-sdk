d4f93be861cebd860fee5d9830cf7a3a0ccccfdb
Jazmine James <jazmine.james@tapad.com>
2014-02-05 19:43:51 -0500

    wait briefly while dispatcher is busy

--- sample-app-with-analytics-v1/src/main/java/com/tapad/sample/TapestryAnalyticsPlugin.java
+++ sample-app-with-analytics-v1/src/main/java/com/tapad/sample/TapestryAnalyticsPlugin.java
@@ -13,35 +13,45 @@ public class TapestryAnalyticsPlugin {
 
     private static void sendAnalytics(GoogleAnalyticsTracker tracker, Map<String,String> analytics) {
         if (!analytics.isEmpty()) {
-            Logging.d("Received analytics: vp : " + analytics.get("vp") +
-                ", pa: " + analytics.get("pa") +
-                ", pt: " + analytics.get("pt") +
-                ", fvp: " + analytics.get("fvp") +
-                ", mrvp: " + analytics.get("mrvp") +
-                ", movp: " + analytics.get("movp"));
             String vp = analytics.get("vp").equals("1") ? analytics.get("vp") + "_Platform" : analytics.get("vp") + "_Platforms";
             String pa = analytics.get("pa").equals("1") ? analytics.get("pa") + "_Platform" : analytics.get("pa") + "_Platforms";
-            // You can modify the custom variables and scope here (2 = session-level scope)
+
             tracker.setCustomVar(1, "Visited_Platforms", vp, 2);
             tracker.trackEvent("tapestry_vp", "Visited_Platforms", "", 0);
             tracker.dispatch();
+            snooze();
             tracker.setCustomVar(1, "Platforms_Associated", pa, 2);
             tracker.trackEvent("tapestry_pa", "Platforms_Associated", "", 0);
             tracker.dispatch();
+            snooze();
             tracker.setCustomVar(1, "Platform_Types", analytics.get("pt"), 2);
             tracker.trackEvent("tapestry_pt", "Platform_Types", "", 0);
             tracker.dispatch();
+            snooze();
             tracker.setCustomVar(1, "First_Visited_Platform", analytics.get("fvp"), 2);
             tracker.trackEvent("tapestry_fvp", "First_Visited_Platform", "", 0);
             tracker.dispatch();
+            snooze();
             tracker.setCustomVar(1, "Most_Recent_Visited_Platform", analytics.get("mrvp"), 2);
             tracker.trackEvent("tapestry_mrvp", "Most_Recent_Visited_Platform", "", 0);
             tracker.dispatch();
             if (analytics.get("movp") != null) {
+                snooze();
                 tracker.setCustomVar(1, "Most_Often_Visited_Platform", analytics.get("movp"), 2);
                 tracker.trackEvent("tapestry_movp", "Most_Often_Visited_Platform", "", 0);
                 tracker.dispatch();
             }
+        }
+    }
+
+    // Necessary to wait briefly while dispatcher is busy
+    private static void snooze(){
+        try {
+            Logging.d("Snoozing for 500 ms");
+            Thread.sleep(500);
+            Logging.d("Wake up");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
