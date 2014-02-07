7518bb263c39d6077fd288c7b89be08a1ed68e6a
Jazmine James <jazmine.james@tapad.com>
2014-02-07 17:18:14 -0500

    analytics plugin v3 WIP

--- sample-google-analytics-v3/src/com/tapad/sample/GoogleAnalyticsActivity.java
+++ sample-google-analytics-v3/src/com/tapad/sample/GoogleAnalyticsActivity.java
@@ -3,38 +3,41 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.GAServiceManager;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
 import com.tapad.tapestry.*;
 
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class GoogleAnalyticsActivity extends Activity {
-    private final String GA_PROPERTY_ID = getMetaData(this, "ga.PROPERTY_ID", null);
+    private String GA_PROPERTY_ID = getMetaData(this, "ga.PROPERTY_ID", "UA-30562281-7");
+
     /**
      * TapestryAnalyticsService can be used from multiple Activities or the Application
      */
     public static class TapestryAnalyticsService {
         private static AtomicLong lastAnalyticsPush = new AtomicLong();
 
-        private static void sendAnalytics(GoogleAnalyticsTracker tracker, Map<String,String> analytics) {
+        private static void sendAnalytics(Tracker tracker, Map<String,String> analytics) {
             if (!analytics.isEmpty()) {
-                String vp = analytics.get("vp").equals("1") ? analytics.get("vp") + "_Platform" : analytics.get("vp") + "_Platforms";
-                String pa = analytics.get("pa").equals("1") ? analytics.get("pa") + "_Platform" : analytics.get("pa") + "_Platforms";
-                // You can modify the custom variables and scope here (2 = session-level scope)
-                tracker.setCustomVar(1, "Visited_Platforms", vp, 2);
-                tracker.setCustomVar(2, "Platforms_Associated", pa, 2);
-                tracker.setCustomVar(3, "Platform_Types", analytics.get("pt"), 2);
-                tracker.setCustomVar(4, "First_Visited_Platform", analytics.get("fvp"), 2);
-                tracker.setCustomVar(5, "Most_Recent_Visited_Platform", analytics.get("mrvp"), 2);
+                String vp = analytics.get("vp").equals("1") ? analytics.get("vp") + " Platform" : analytics.get("vp") + " Platforms";
+                String pa = analytics.get("pa").equals("1") ? analytics.get("pa") + " Platform" : analytics.get("pa") + " Platforms";
+                tracker.set(Fields.customDimension(1), vp); // Visited Platforms
+                tracker.set(Fields.customDimension(2), pa); // Platforms Associated
+                tracker.set(Fields.customDimension(3), analytics.get("pt")); // Platform Types
+                tracker.set(Fields.customDimension(4), analytics.get("fvp")); // First Visited Platform
+                tracker.set(Fields.customDimension(5), analytics.get("mrvp")); // Most Recent Visited Platform
                 if (analytics.get("movp") != null)
-                    tracker.setCustomVar(6, "Most_Often_Visited_Platform", analytics.get("movp"), 2);
-                tracker.trackEvent("tapestry", "android", "", 0);
-                tracker.dispatch();
+                    tracker.set(Fields.customDimension(6), analytics.get("movp")); // Most Often Visited Platform
+                // manual dispatch
+                GAServiceManager.getInstance().dispatchLocalHits();
             }
         }
 
-        public static void track(final GoogleAnalyticsTracker tracker, TapestryClient tapestry) {
+        public static void track(final Tracker tracker, TapestryClient tapestry) {
             boolean isNewSession = lastAnalyticsPush.get() < System.currentTimeMillis() - 30 * 60 * 1000;
             lastAnalyticsPush.set(System.currentTimeMillis());
             tapestry.send(new TapestryRequest().analytics(isNewSession), new TapestryCallback() {
@@ -49,17 +52,10 @@ public class GoogleAnalyticsActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GoogleAnalyticsTracker tracker = GoogleAnalyticsTracker.getInstance();
-        tracker.startNewSession(GA_PROPERTY_ID, this);
-        tracker.setDebug(true);
+        Tracker tracker = GoogleAnalytics.getInstance(this).getTracker(GA_PROPERTY_ID);
         TapestryAnalyticsService.track(tracker, new TapestryClient(this));
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        GoogleAnalyticsTracker.getInstance().stopSession();
-    }
 
     private static String getMetaData(Context context, String key, String defaultValue) {
         try {
