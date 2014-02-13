42b2872651fb1a238b264b22b94b8cef7451f9c8
Jazmine James <jazmine.james@tapad.com>
2014-02-13 17:46:12 -0500

    dimension indices are configured in AndroidManifest.xml

--- sample-google-analytics-v3/src/com/tapad/sample/GoogleAnalyticsActivity.java
+++ sample-google-analytics-v3/src/com/tapad/sample/GoogleAnalyticsActivity.java
@@ -1,5 +1,8 @@
 package com.tapad.sample;
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
 import com.google.analytics.tracking.android.*;
 import com.tapad.tapestry.*;
 
@@ -7,13 +10,6 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class GoogleAnalyticsActivity extends Activity {
-    // Custom Dimension indices that will be used for Tapestry Analytics
-    private static final int VISITED_PLATFORMS_DIM_IDX = 1;
-    private static final int PLATFORMS_ASSOC_DIM_IDX = 2;
-    private static final int PLATFORM_TYPES_DIM_IDX = 3;
-    private static final int FIRST_VISITED_DIM_IDX = 4;
-    private static final int MOST_RECENT_DIM_IDX = 5;
-    private static final int MOST_OFTEN_DIM_IDX = 6;
 
     @Override
     public void onStart() {
@@ -26,7 +22,7 @@ public class GoogleAnalyticsActivity extends Activity {
         tracker.activityStart(this);
 
         // Send Tapestry analytics data
-        TapestryAnalyticsService.track(tracker, new TapestryClient(this));
+        TapestryAnalyticsService.track(tracker, new CustomDimensionConfig(), new TapestryClient(this));
     }
 
     @Override
@@ -43,7 +39,7 @@ public class GoogleAnalyticsActivity extends Activity {
     public static class TapestryAnalyticsService {
         private static AtomicLong lastAnalyticsPush = new AtomicLong();
 
-        private static void sendAnalytics(Tracker tracker, Map<String,String> analytics) {
+        private static void sendAnalytics(Tracker tracker, CustomDimensionConfig dimensions, Map<String,String> analytics) {
             if (!analytics.isEmpty()) {
                 String vp = analytics.get("vp").equals("1") ? analytics.get("vp") + " Platform" : analytics.get("vp") + " Platforms";
                 String pa = analytics.get("pa").equals("1") ? analytics.get("pa") + " Platform" : analytics.get("pa") + " Platforms";
@@ -53,35 +49,57 @@ public class GoogleAnalyticsActivity extends Activity {
                 String movp = analytics.get("movp");
 
                 tracker.send(MapBuilder.createEvent("Tapestry", "Visited Platforms", vp, null)
-                        .set(Fields.customDimension(VISITED_PLATFORMS_DIM_IDX), vp)
+                        .set(Fields.customDimension(dimensions.VISITED_PLATFORMS_DIM_IDX), vp)
                         .build());
                 tracker.send(MapBuilder.createEvent("Tapestry", "Platforms Associated", pa, null)
-                        .set(Fields.customDimension(PLATFORMS_ASSOC_DIM_IDX), pa)
+                        .set(Fields.customDimension(dimensions.PLATFORMS_ASSOC_DIM_IDX), pa)
                         .build());
                 tracker.send(MapBuilder.createEvent("Tapestry", "Platform Types", pt, null)
-                        .set(Fields.customDimension(PLATFORM_TYPES_DIM_IDX), pt)
+                        .set(Fields.customDimension(dimensions.PLATFORM_TYPES_DIM_IDX), pt)
                         .build());
                 tracker.send(MapBuilder.createEvent("Tapestry", "First Visited Platform", fvp, null)
-                        .set(Fields.customDimension(FIRST_VISITED_DIM_IDX), fvp)
+                        .set(Fields.customDimension(dimensions.FIRST_VISITED_DIM_IDX), fvp)
                         .build());
                 tracker.send(MapBuilder.createEvent("Tapestry", "Most Recent Visited Platform", mrvp, null)
-                        .set(Fields.customDimension(MOST_RECENT_DIM_IDX), mrvp)
+                        .set(Fields.customDimension(dimensions.MOST_RECENT_DIM_IDX), mrvp)
                         .build());
                 tracker.send(MapBuilder.createEvent("Tapestry", "Most Often Visited Platform", movp, null)
-                        .set(Fields.customDimension(MOST_OFTEN_DIM_IDX), movp)
+                        .set(Fields.customDimension(dimensions.MOST_OFTEN_DIM_IDX), movp)
                         .build());
             }
         }
 
-        public static void track(final Tracker tracker, TapestryClient tapestry) {
+        public static void track(final Tracker tracker, final CustomDimensionConfig dimensions, TapestryClient tapestry) {
             boolean isNewSession = lastAnalyticsPush.get() < System.currentTimeMillis() - 30 * 60 * 1000;
             lastAnalyticsPush.set(System.currentTimeMillis());
             tapestry.send(new TapestryRequest().analytics(isNewSession), new TapestryCallback() {
                 @Override
                 public void receive(TapestryResponse response, Exception exception, long millisSinceInvocation) {
-                    sendAnalytics(tracker, response.analytics());
+                    sendAnalytics(tracker, dimensions, response.analytics());
                 }
             });
+        }
+    }
+
+    // Custom Dimension indices that will be used for Tapestry Analytics
+    private class CustomDimensionConfig {
+        // If one or more of the below fields are omitted from AndroidManifest.xml,
+        // the variable(s) will be sent as custom dimension index = 0,
+        // which will be ignored by Google Analytics
+        private final int VISITED_PLATFORMS_DIM_IDX = Integer.parseInt(getMetaData(GoogleAnalyticsActivity.this, "ga.VISITED_PLATFORMS_DIM_IDX", "0"));
+        private final int PLATFORMS_ASSOC_DIM_IDX = Integer.parseInt(getMetaData(GoogleAnalyticsActivity.this, "ga.PLATFORMS_ASSOC_DIM_IDX", "0"));
+        private final int PLATFORM_TYPES_DIM_IDX = Integer.parseInt(getMetaData(GoogleAnalyticsActivity.this, "ga.PLATFORM_TYPES_DIM_IDX", "0"));
+        private final int FIRST_VISITED_DIM_IDX = Integer.parseInt(getMetaData(GoogleAnalyticsActivity.this, "ga.FIRST_VISITED_DIM_IDX", "0"));
+        private final int MOST_RECENT_DIM_IDX = Integer.parseInt(getMetaData(GoogleAnalyticsActivity.this, "ga.MOST_RECENT_DIM_IDX", "0"));
+        private final int MOST_OFTEN_DIM_IDX = Integer.parseInt(getMetaData(GoogleAnalyticsActivity.this, "ga.MOST_OFTEN_DIM_IDX", "0"));
+    }
+
+    private static String getMetaData(Context context, String key, String defaultValue) {
+        try {
+            String value = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA).metaData.get(key).toString();
+            return value == null ? defaultValue : value;
+        } catch (Exception e) {
+            return defaultValue;
         }
     }
 }
